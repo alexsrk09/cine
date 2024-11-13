a71e34b00abbfbe05329527a484a0eb41dd57682
Alex JL <alexsrk09@gmail.com>
2024-11-13 17:48:20 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,4 @@ Route::post('/updatesilla', [SillaController::class, 'update']);
 Route::get('/getallsalas', [SalaController::class, 'getAll']);
 Route::get('getsillas/{id}', [SillaController::class, 'getSillas']);
 Route::get('/prueba', function () {return view('/prueba');});
-Route::get('/token', function () {return csrf_token(); });
+// Route::get('/token', function () {return csrf_token(); });
