160f3176a6199fc8144c669367fc00531a960416
alnokio <arv0033@alu.medac.es>
2024-11-14 20:09:03 +0100

    antonio fetch SALAS

--- public/js/prueba2.js
+++ public/js/prueba2.js
@@ -1,34 +1,55 @@
-// Recoger datos del json, a partir de la ruta localhost:8000/getallsalas
-
-const url = 'salas.json'
-// const url2 = 'localhost:8000/getallsalas'
+const url = '/getallsalas'
 
 fetch(url)
-    .then(respuesta => {
-        // Aquí le pongo para devolver cada sala, nombre y asientos
-        console.log(respuesta)
-        console.log(respuesta.status)
-        console.log(respuesta.statusText)
-        console.log(respuesta.url)
+    .then(respuesta => respuesta.json())
+    .then(result => mostrar(result))
+    .catch(error => console.log(error))
 
-        // Retornamos la respuesta en texto para qu epueda ser procesada en el siguiente .then
-        return respuesta.text()
-    })
 
-    //Desestructuramos objeto
-    function mostrarDatos({id, nombre, created_at, updated_at}){
-        console.log(id)
-        console.log(nombre)
-        console.log(created_at)
-        console.log(updated_at)
-    }
+function mostrar(datos) {
 
-    // Recoger datos del json donde las salas
-    function mostrarIds(resultado) {
 
-        resultado.forEach(sala => {
-            // Aplicamos destructuración
-            const {id, nombre, created_at, updated_at} = sala
-            console.log(id)
+    const salas = document.querySelector('#salas')
+    datos.forEach(sala => {
+        const { id, nombre, created_at, updated_at } = sala
+        // console.log("ID: " + id)
+        // console.log("Nombre sala: " + nombre)
+        // console.log("Creada: " + created_at)
+        // console.log("Actualizada: " + updated_at)
+        // console.log("")
+        let salaDIV = document.createElement('div')
+        salaDIV.style.backgroundColor = 'green'
+        salaDIV.style.margin = '5px'
+        salaDIV.style.height = '30px'
+        salaDIV.textContent = nombre;
+        salas.appendChild(salaDIV)
+
+        salaDIV.addEventListener('click', () => {
+
+            fetch('/getsillas/' + id)
+                .then(respuesta => respuesta.json())
+                .then(result => mostrarSillas(result))
+                .catch(error => console.log(error))
         })
-    }
+    })
+}
+
+function mostrarSillas(datos) {
+    const pelisDIV = document.querySelector('#pelis')
+
+    pelisDIV.textContent = ""
+    let contador = 0
+
+    datos.forEach(silla => {
+        contador++
+        const {id, ocupada} = silla;
+        let cuadrado = document.createElement('div')
+        cuadrado.textContent = contador
+        pelisDIV.appendChild(cuadrado)
+    })
+}
+
+
+// mandar header y body
+//
+// {{csrf_token()}}
