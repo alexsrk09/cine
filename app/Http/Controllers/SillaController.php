ab9a6ca66e837fc9188a6bbf5c861b27c3148a3f
aqp0001 <aqp0001@alu.medac.es>
2024-11-15 15:05:41 +0100

    Validaciones terminadas
    validaciones empezadas y acabadas

--- app/Http/Controllers/SillaController.php
+++ app/Http/Controllers/SillaController.php
@@ -4,41 +4,91 @@ namespace App\Http\Controllers;
 
 use App\Models\Silla;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class SillaController extends Controller
 {
     public function create(Request $request){
-        /////////////////////////////////////////////////////////////////////////
-        // comprobar si $request->id es un numero y existe en la base de datos
-        /////////////////////////////////////////////////////////////////////////
-        $silla = new Silla();
-        $silla->sala_id = $request->sala_id;
-        $silla->save();
-        return $silla;
+
+        $data =[
+            "id_silla" => $request->id,
+        ];
+
+        $validator = Validator::make($data,
+        [
+            'id_silla' => 'required|integer|max:100',
+        ],
+        []);
+
+        if($validator -> passes() && $request -> id != -1){
+            $silla = new Silla();
+            $silla->sala_id = $request->sala_id;
+            $silla->save();
+            return $silla;
+        }
+        else{
+            return "El ID no es valido";
+        }
     }
     // Route::get('getsillas/{id}', [SillaController::class, 'getSillas']);
     public function getSillas($id){
-        /////////////////////////////////////////////////////////////////////////
-        // comprobar si $id es un numero y existe en la base de datos
-        /////////////////////////////////////////////////////////////////////////
-        $data = Silla::where('sala_id', $id)->get();
-        return $data;
+
+        $data =[
+            "id_silla" => $id,
+        ];
+
+        $validator = Validator::make($data,
+        [
+            'id_silla' => 'required|integer|max:100',
+        ],
+        []);
+
+        if($validator -> passes() && $id != -1){
+            $datos = Silla::where('sala_id', $id)->get();
+            return $datos;
+        }else{
+            return 'El ID no es valido';
+        }
     }
     public function update(Request $request){
-        /////////////////////////////////////////////////////////////////////////
-        // comprobar si $request->id es un numero y existe en la base de datos
-        /////////////////////////////////////////////////////////////////////////
-        $silla = Silla::find($request->id);
-        $silla->ocupada = !$silla->ocupada;
-        $silla->save();
-        return $silla;
+
+        $data =[
+            "id_silla" => $request->id,
+        ];
+
+        $validator = Validator::make($data,
+        [
+            'id_silla' => 'required|integer|max:100',
+        ],
+        []);
+
+        if($validator -> passes() && $request -> id != -1){
+            $silla = Silla::find($request->id);
+            $silla->ocupada = !$silla->ocupada;
+            $silla->save();
+            return $silla;
+        }else{
+            return 'El ID no es valido';
+        }
     }
     public function delete(Request $request){
-        /////////////////////////////////////////////////////////////////////////
-        // comprobar si $request->id es un numero y existe en la base de datos
-        /////////////////////////////////////////////////////////////////////////
-        $silla = Silla::find($request->id);
-        $silla->delete();
-        return $silla;
+
+        $data =[
+            "id_silla" => $request->id,
+        ];
+
+        $validator = Validator::make($data,
+        [
+            'id_silla' => 'required|integer|max:100',
+        ],
+        []);
+
+        if($validator -> passes() && $request -> id != -1){
+            $silla = Silla::find($request->id);
+            $silla->delete();
+            return $silla;
+        }else{
+            return 'El ID no es valido';
+        }
     }
 }
