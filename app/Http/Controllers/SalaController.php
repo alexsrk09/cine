bf8a19e33b44d942f13ca96689037efd2020f274
aqp0001 <aqp0001@alu.medac.es>
2024-11-17 12:24:04 +0100

    Validacion create
    Validacion terminada

--- app/Http/Controllers/SalaController.php
+++ app/Http/Controllers/SalaController.php
@@ -12,11 +12,31 @@ class SalaController extends Controller
     // crud Model Sala
     public function create()
     {
-        $sala = new Sala();
-        $sala->nombre = $_POST['nombre'];
-        $sala->descripcion = $_POST['descripcion'];
-        $sala->save();
-        return $sala;
+        if(!isset($_POST['descripcion']) || strlen($_POST['descripcion']) == 0){
+            $_POST['descripcion'] = "descripcion vacia";
+        }
+
+        $data =[
+            'nombre' => $_POST['nombre'],
+            'descripcion' => $_POST['descripcion']
+        ];
+
+        $validator = Validator::make($data,
+        [
+            'nombre' => 'required|string|max:100',
+            'descripcion' => 'string|max:1000'
+        ],
+        []);
+
+        if($validator ->passes()){
+            echo "create";
+            $sala = new Sala();
+            $sala->nombre = $_POST['nombre'];
+            $sala->save();
+            return $sala;
+        }else{
+            return "Los datos introducidos son errones";
+        }
     }
     public function getAll(){
         return Sala::all();
